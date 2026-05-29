ed6864eebcb1bb7113bd5f46483e8e36de9f219c
GuuStav0 <luizg600@gmail.com>
2026-05-29 09:05:46 -0300

    Reestruturação das source files

---
+++ src/assets/view/about.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./about.css"
+
+function AboutUs(){
+    const [] = useState();
+
+    return(
+        <>
+        </>
+    )
+}
+
+export default AboutUs
